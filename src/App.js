2294d2e5d4ab29f230a29adba76cad283976594f
saraogles <sara@untetherllc.com>
2021-07-01 16:51:14 -0500

    final for the week

--- src/App.js
+++ src/App.js
@@ -842,40 +842,52 @@ function Team() {
   return (
 
     <div className="">
-    <div className="bg-bluegray-900">
+
+    <div className="">
+
       <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8 lg:flex lg:justify-between">
+
         <div className="max-w-xl">
-          <h2 className="text-4xl font-extrabold text-bluegray-100 sm:text-5xl sm:tracking-tight lg:text-6xl pb-4">
-            Meet the team
+
+          <h2 className="text-4xl font-extrabold text-yellow-400 sm:text-5xl sm:tracking-wide lg:text-6xl pb-4">
+            The Team
           </h2>
-          <p className="mt-5 text-xl text-bluegray-300">
-            Start building for free, then add a site plan to go live. Account plans unlock additional features.
+          <p className="mt-2 text-xl text-bluegray-300">
+            Meet the people who make Untether a better place to be.
           </p>
         </div>
       </div>
     </div>
-      <div className="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-24">
-        <div className="space-y-12">
-          <div className="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
-            <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl text-yellow-400">Meet the team</h2>
-            <p className="text-xl text-bluegray-300 pb-8">
-              Ornare sagittis, suspendisse in hendrerit quis. Sed dui aliquet lectus sit pretium egestas vel mattis
-              neque.
-            </p>
+
+      <div className="border-t-2 border-bluegray-500 border-b-2 border-bluegray-500 max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-16">
+        <div className="space-y-24">
+
+        <div className="invisible sm:visible absolute -right-56 -mt-48">
+          <div className="w-160 h-160 lg:w-240 lg:h-240 bg-red-900 rounded-full opacity-50">
           </div>
+        </div>
+
+          <div className="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
+            <p className="text-xl text-bluegray-200">
+              "Great things in business are never done by one person. They're done by a team of people."
+            </p>
+            <p className="italic text-bluegray-400">- Steve Jobs</p>
+          </div>
+
           <ul className="mx-auto space-y-16 sm:grid sm:grid-cols-2 sm:gap-16 sm:space-y-0 lg:grid-cols-3 lg:max-w-5xl">
+
             {people.map((person) => (
               <li key={person.name}>
                 <div className="space-y-6">
                   <img className="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56" src={person.imageUrl} alt="" />
                   <div className="space-y-2">
                     <div className="text-lg leading-6 font-medium space-y-1">
-                      <h3>{person.name}</h3>
-                      <p className="text-indigo-600">{person.role}</p>
+                      <h3 className="text-bluegray-100">{person.name}</h3>
+                      <p className="p-2 text-yellow-400">{person.role}</p>
                     </div>
-                    <ul className="flex justify-center space-x-5">
+                    <ul className="p-2 flex justify-center space-x-5">
                       <li>
-                        <a href={person.twitterUrl} className="text-gray-400 hover:text-gray-500">
+                        <a href={person.twitterUrl} className="text-bluegray-400 hover:text-yellow-400">
                           <span className="sr-only">Twitter</span>
                           <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
@@ -883,7 +895,7 @@ function Team() {
                         </a>
                       </li>
                       <li>
-                        <a href={person.linkedinUrl} className="text-gray-400 hover:text-gray-500">
+                        <a href={person.linkedinUrl} className="text-bluegray-400 hover:text-yellow-400">
                           <span className="sr-only">LinkedIn</span>
                           <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                             <path
